11d5a0577f3bc70502e8f962ea9bca68473b6d11
PrabhuUdurg <egorpeshiy@gmail.com>
2023-04-20 22:07:15 +0200

    beginning chop func

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from playsound import playsound
 from pydub import AudioSegment
+import random
+
+track = []
 
 
 def chop_sound(file):
     sound = AudioSegment.from_file(file)
-    ten_seconds = 10 * 1000
-    first_10_seconds = sound[:ten_seconds]
-    # first_half.export("/nosie-machine/first_half.wav", format="wav")
-    sound.export("mashup.mp3", format="mp3")
-    playsound(sound)
+    ten_seconds = random.randrange(1, 30) * 1000
+    # begining_of_file = random.randrange(1, 30) * 1000
+    sound = sound[:ten_seconds]
+    # sound = sound[begining_of_file]
+    sound.export('new_file', format="wav")
+    playsound('new_file')
 
 
 chop_sound('test1.wav')
