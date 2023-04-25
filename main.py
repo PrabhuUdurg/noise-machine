dd9a77549960952330e6c170dc57b85ea28e0e73
Prabhu <egorpeshiy@gmail.com>
2023-04-25 11:57:33 +0200

    chop from beginning and finish

--- main.py
+++ main.py
@@ -7,11 +7,20 @@ track = []
 
 def chop_sound(file):
     sound = AudioSegment.from_file(file)
-    ten_seconds = random.randrange(1, 30) * 1000
-    # begining_of_file = random.randrange(1, 30) * 1000
-    sound = sound[:ten_seconds]
-    # sound = sound[begining_of_file]
-    sound.export('new_file', format="wav")
+
+# Assign range of cut
+    first_seconds = random.randrange(1, 30) * 1000
+    last_seconds = random.radrange(1, 10) * 1000 
+
+# Chop beginning and ending  
+    beginning = sound[:ten_seconds]
+    end = sound[-last_seconds:]
+
+# Editing main sound
+    whole_sound = sound - beginning - end 
+    
+    
+    whole_sound.export('new_file', format="wav")
     playsound('new_file')
 
 
